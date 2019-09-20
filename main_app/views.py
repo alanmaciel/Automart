5af4b70db0c92573564fc48a167a94f0bb321c74
Alan Maciel <alan.maciel.salcedo@gmail.com>
2019-09-20 15:11:50 -0500

    Usando objetos para desplegar datos

--- main_app/views.py
+++ main_app/views.py
@@ -2,11 +2,21 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-  name = "Ford focus"
-  model = "2015"
-  value = 50000.00
-  context = { 'auto_name': name,
-              'auto_model': model,
-              'auto_value': value }
+  return render(request, 'index.html', {'autos':autos})
 
-  return render(request, 'index.html', context)
+class Auto:
+  def __init__(self, nombre, valor, km, modelo, color, localizacion):
+    self.nombre = nombre
+    self.valor = valor
+    self.km = km
+    self.modelo = modelo
+    self.color = color
+    self.localizacion = localizacion
+
+
+autos = [
+  Auto('Ford Focus', 50000, 88000, 2015, 'blanco', "leon" ),
+  Auto('Porsche', 1500000, 48000, 2017, 'gris', "queretaro" ),
+  Auto('Batimovil', 0, 128000, 1963, 'negro', "california" ),
+  Auto('Lexus', 560000, 12000, 2019, 'rojo', "leon" )
+]
