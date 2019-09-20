2a34b35837e58f4c85742a851dddc2726c9ca42e
Alan Maciel <alan.maciel.salcedo@gmail.com>
2019-09-20 14:43:38 -0500

    Usando datos dinamicos desde el view

--- main_app/views.py
+++ main_app/views.py
@@ -2,4 +2,11 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-  return render(request, 'index.html')
+  name = "Ford focus"
+  model = "2015"
+  value = 50000.00
+  context = { 'auto_name': name,
+              'auto_model': model,
+              'auto_value': value }
+
+  return render(request, 'index.html', context)
