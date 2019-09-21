f5c629e546987d7c0d17af0b8584ea94a4af4f2f
Alan Maciel <alan.maciel.salcedo@gmail.com>
2019-09-20 23:21:27 -0500

    Crea Auto model, modifica la vista para usar modelos

--- main_app/views.py
+++ main_app/views.py
@@ -1,23 +1,7 @@
 from django.shortcuts import render
+from .models import Auto
 
 # Create your views here.
 def index(request):
-  return render(request, 'index.html', {'autos':autos})
-
-class Auto:
-  def __init__(self, nombre, valor, km, modelo, color, localizacion, img_url):
-    self.nombre = nombre
-    self.valor = valor
-    self.km = km
-    self.modelo = modelo
-    self.color = color
-    self.localizacion = localizacion
-    self.img_url = img_url
-
-
-autos = [
-  Auto('Ford Focus', 50000, 88000, 2015, 'blanco', "leon", "https://http2.mlstatic.com/ford-focus-2015-se-D_NQ_NP_149425-MLM25459576804_032017-F.jpg" ),
-  Auto('Porsche', 1500000, 48000, 2017, 'gris', "queretaro", "https://m.atcdn.co.uk/vms/media/w1366/23d757c5424f485f92db7f76b028a2cf.jpg" ),
-  Auto('Batimovil', 0, 128000, 1963, 'negro', "california", "https://vignette.wikia.nocookie.net/batman/images/8/80/1960%27s_TV_BAtmobile_01.jpg/revision/latest/scale-to-width-down/700?cb=20090609183446"),
-  Auto('Lexus', 560000, 12000, 2019, 'rojo', "leon", "https://cmsimages-alt.kbb.com/content/dam/kbb-editorial/make/lexus/lc/2018/lc-500-kbb/01-2018-Lexus-LC-500-Action-KBB.jpg" )
-]
+  autos = Auto.objects.all()
+  return render(request, 'index.html', {'autos' : autos})
