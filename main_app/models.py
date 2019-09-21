83562ec8c30e6f255642b64e0a73e8b07f98ec2e
Alan Maciel <alan.maciel.salcedo@gmail.com>
2019-09-20 22:54:37 -0500

    Actualizando foto de ford focus y arreglando margen inferior del hero

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Auto(models.Model):
+
+  nombre = models.CharField(max_length=100)
+  valor = models.DecimalField(max_digits=10, decimal_places=2)
+  km = models.CharField(max_length=100)
+  modelo = models.CharField(max_length=100)
+  color = models.CharField(max_length=100)
+  localizacion = models.CharField(max_length=100)
+  img_url = models.CharField(max_length=100)
