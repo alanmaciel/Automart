8cbb718a8dda4e849f7248ab018da00a8c29958c
Alan Maciel <alan.maciel.salcedo@gmail.com>
2019-09-28 02:04:25 -0500

    Agrega funcion show a views

--- main_app/views.py
+++ main_app/views.py
@@ -5,3 +5,7 @@ from .models import Auto
 def index(request):
   autos = Auto.objects.all()
   return render(request, 'index.html', {'autos' : autos})
+
+def show(request, auto_id):
+  auto = Auto.objects.get(id=auto_id)
+  return render(request, 'show.html', {'auto' : auto})
