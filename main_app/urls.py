32435b45dda8de8b4021fb40b25ecd4c958f22ce
Alan Maciel <alan.maciel.salcedo@gmail.com>
2019-09-28 02:04:02 -0500

    Agrega ruta para el detail page

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,6 +1,12 @@
-from django.urls import path
+from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    path('', views.index),
+    url(r'^$', views.index), 
+    url(r'^([0-9]+)/$', views.show),
 ]
+
+# [0-9]+ hace match con cualquier numero
+# los parentesis capturan el valor. 
+# al capturar el valor este se pasa como parametro
+# al detail view, ej. localhost/10
